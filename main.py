aee3192dc111506404f8685a69d2064f89ea440f
Максимилиан <pavlovmaksimilian1@gmail.com>
2023-07-30 21:51:57 +0300

    First submission

---
+++ main.py
@@ -0,0 +1,77 @@
+import optuna
+import pandas as pd
+
+from src.opt import Supply_chain_optimization
+
+bio_hist = pd.read_csv('data/Biomass_history.csv')
+dist_matrix = pd.read_csv('data/Distance_matrix.csv', header=None, usecols=[i for i in range(1, (2419))])
+sample_submit = pd.read_csv('data/sample_submission.csv')
+dist_matrix = dist_matrix.iloc[1:]
+
+dist_arr = dist_matrix.values.copy()
+opt = Supply_chain_optimization(dist_arr)
+
+bio_hist = opt.biomass_forecast(bio_hist)
+bio_hist['2018'] = bio_hist['2018'].apply(lambda x: x if x > 0 else 0)
+bio_hist['2019'] = bio_hist['2019'].apply(lambda x: x if x > 0 else 0)
+
+# mean between 2018 and 2019
+last_time_distr = (bio_hist['2018'] + bio_hist['2019']) / 2
+
+n_ref, n_depots = opt.calc_num_objects(last_time_distr)
+
+
+def optuna_obj(trial):
+    values_dep = [trial.suggest_int('depot_idx_{}'.format(i), 0, len(dist_arr) - 1) for i in range((n_depots))]
+    values_ref = [trial.suggest_int('ref_idx_{}'.format(i), 0, len(dist_arr) - 1) for i in range((n_ref))]
+
+    opt = Supply_chain_optimization(dist_arr)
+
+    sum_cost_depot = opt.calculate_depot_sum(last_time_distr, values_dep)
+
+    sum_cost_ref = opt.refinery_obj(last_time_distr, values_ref)
+
+    return sum_cost_depot + sum_cost_ref
+
+
+def get_submission_file(study, path_to_save='submissions/sub1.csv'):
+    locations_df = pd.DataFrame.from_dict(study.best_params, orient='index')
+    ref_idx = [locations_df.loc[x][0] for x in list(locations_df.index) if 'ref' in x]
+    depot_idx = [locations_df.loc[x][0] for x in list(locations_df.index) if 'depot' in x]
+
+    opt = Supply_chain_optimization(dist_arr)
+
+    sum_cost_depot = opt.calculate_depot_sum(last_time_distr, depot_idx)
+    sum_cost_ref = opt.refinery_obj(last_time_distr, ref_idx)
+
+    print('Sum cost of optimization': sum_cost_depot + sum_cost_ref)
+
+    final_df = opt.final_df.copy()
+
+    final_loc = opt.final_df.query("data_type == 'refinery_location' or data_type =='depot_location'")
+    final_loc['year'] = 20182019
+
+    # 2018 and 2019 predictions
+    dd_2018 = opt.final_df.query("data_type == 'biomass_demand_supply' or data_type =='pallet_demand_supply'")
+    dd_2018['year'] = 2018
+    dd_2019 = opt.final_df.query("data_type == 'biomass_demand_supply' or data_type =='pallet_demand_supply'")
+    dd_2019['year'] = 2019
+
+    biomass_forecast_2018 = bio_hist[['Index', '2018']].rename(columns={'Index': 'source_index', '2018': 'value'})
+    biomass_forecast_2018['data_type'] = 'biomass_forecast'
+    biomass_forecast_2018['year'] = 2018
+    biomass_forecast_2019 = bio_hist[['Index', '2019']].rename(columns={'Index': 'source_index', '2019': 'value'})
+    biomass_forecast_2019['data_type'] = 'biomass_forecast'
+    biomass_forecast_2019['year'] = 2019
+
+    submission_df = pd.concat([final_loc, biomass_forecast_2018, biomass_forecast_2019, dd_2018, dd_2019])
+    submission_df = submission_df[['year', 'data_type', 'source_index', 'destination_index', 'value']]
+    submission_df.to_csv(path_to_save)
+    return submission_df
+
+
+if __name__ == 'main':
+    study = optuna.create_study()  # Create a new study.
+    study.optimize(optuna_obj, n_trials=300)
+
+    get_submission_file(study)
--- src/opt.py
+++ src/opt.py
@@ -1,10 +1,159 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 
-def calc_num_objects(hist: pd.Series):
-    n_ref = hist.sum() // 10e4
-    n_depots = hist.sum() // 20e3
-    clip_ref = np.clip(n_ref, 0, 5)
-    clip_depots = np.clip(n_depots, 0, 25)
-    return int(clip_ref), int(clip_depots)
+class Supply_chain_optimization:
+
+    def __init__(self, dist_matrix):
+        '''
+
+        dist_matrix: np.array
+        distance matrix
+
+
+        '''
+        self.DEPOT_LIMIT = 2e4
+        self.REF_LIMIT = 1e5
+        self.TOL = 200
+      #  self.hist = history
+        self.dist_arr = dist_matrix.copy()
+
+        self.final_df = pd.DataFrame(columns = ['data_type', 'source_index', 'destination_index', 'value'])
+    @staticmethod
+    def calc_num_objects(hist: pd.Series):
+        '''
+        Calculate number of refinaries and depots based on sum values of biomass
+
+        '''
+        n_ref = hist.sum() // 10e4
+        n_depots = hist.sum() // 20e3
+        clip_ref = np.clip(n_ref, 0, 5)
+        clip_depots = np.clip(n_depots, 0, 25)
+        return int(clip_ref) + 1, int(clip_depots) + 1
+
+    def calculate_depot_sum(self, dep_index, hist):
+        distance_arr = self.dist_arr.copy()
+        depot_sum = {}
+        depot_cost = {}
+        for depot in dep_index:
+            depot_sum[depot] = hist.iloc[depot]
+            depot_cost[depot] = 0
+            distance_arr[depot, :] = 1e6
+
+            # depot location
+            self.final_df = pd.concat([self.final_df, pd.DataFrame([['depot_location', depot, None, None]],
+                                                           columns=self.final_df.columns)], axis = 0)
+
+
+            while depot_sum[depot] < self.DEPOT_LIMIT - self.TOL:
+
+                arg_indx = np.argmin(distance_arr[:, depot])
+
+                # pallet_demand_supply
+                self.final_df = pd.concat([self.final_df, pd.DataFrame([['pallet_demand_supply', depot, arg_indx,
+                                                                hist.iloc[arg_indx]]],
+                                                                columns=self.final_df.columns)], axis=0)
+
+
+                if distance_arr[arg_indx, depot] == 1e6:
+                    break
+
+                depot_cost[depot] += distance_arr[arg_indx, depot] * hist.iloc[arg_indx]
+                distance_arr[arg_indx, :] = 1e6
+
+                depot_sum[depot] += hist.iloc[arg_indx]
+            #   biomass_arr[arg_indx] = 0
+
+
+        sums = [val for val in list(depot_sum.values()) if val > self.DEPOT_LIMIT]
+
+        self.depot_sum = depot_sum
+
+        sum_cost = np.array([value for value in depot_cost.values()]).sum()
+
+        if len(sums) > 0:
+            sum_cost += 1e5*len(sums)
+
+        return sum_cost
+
+    def refinery_obj(self, ref_index):
+
+        depots = list(self.depot_sum.keys())
+        depots_dict = self.depot_sum.copy()
+
+        distance_arr = self.dist_arr.copy()
+        bioref_sum = {}
+        bioref_cost = {}
+        for bf in ref_index:
+            # bioferinery location
+
+            self.final_df = pd.concat([self.final_df, pd.DataFrame([['refinery_location', bf, None, None]],
+                                                                   columns=self.final_df.columns)], axis=0)
+            done = False
+
+            bioref_sum[bf] = 0
+            bioref_cost[bf] = 0
+            dep_sum = np.array([val for val in depots_dict.values()]).sum()
+
+            # print(len(depots))
+            while not done and len(depots) > 0:
+                dist_min = 1e6
+                dep_opt = 0
+                for i, dep in enumerate(depots):
+
+                    dep_bf_dist = distance_arr[dep, bf]
+                    if dep_bf_dist < dist_min:
+                        dist_min = dep_bf_dist
+                        dep_opt = dep
+                        dep_idx = i
+
+                # biomass_demand_supply
+                self.final_df = pd.concat([self.final_df, pd.DataFrame([['biomass_demand_supply', dep_opt, bf, depots_dict[dep_opt]]],
+                                                                       columns=self.final_df.columns)], axis=0)
+
+                depots.pop(dep_idx)
+                bioref_cost[bf] += distance_arr[dep_opt, bf] * depots_dict[dep_opt]
+
+                bioref_sum[bf] += depots_dict[dep_opt]
+
+                if bioref_sum[bf] > self.REF_LIMIT - 1e3:
+                    done = True
+
+                depots_dict[dep_opt] = 0
+
+
+            self.bioref_sum = bioref_sum
+            sum_cost = np.array([value for value in bioref_cost.values()]).sum()
+        return sum_cost
+
+    def biomass_forecast(self, bio_hist):
+
+        lr = LinearRegression()
+
+        x = np.arange(0,8).reshape(-1,1)
+
+        pred_list = []
+        for i in range(0, len(bio_hist)):
+            y = bio_hist.iloc[i, 3:].values.reshape(-1, 1)
+            lr = LinearRegression().fit(x, y)
+            preds = lr.predict(np.array([8, 9]).reshape(-1, 1))
+            pred_list.append(list(preds))
+
+        pred_df = pd.DataFrame(np.array(pred_list).squeeze(), columns=['2018', '2019'],
+                               index=np.arange(0, len(bio_hist)))
+
+        bio_hist = pd.concat([bio_hist, pred_df], axis=1)
+        return bio_hist
+
+
+
+
+
+
+
+
+
+
+
+
